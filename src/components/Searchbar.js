5ce71911737ddb89a37118cdb820ba45cde9d8f3
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2021-06-16 21:47:31 +0100

    feature: add searchbar

---
+++ src/components/Searchbar.js
@@ -0,0 +1,15 @@
+import { Box, Input, Icon, Stack } from '@chakra-ui/react';
+import { Search2Icon } from '@chakra-ui/icons';
+
+const Searchbar = () => {
+	return (
+		<Stack margin="0" direction="row" mt="10">
+			<Box backgroundColor="#F4F6FB" px="2" py="2" rounded="md">
+				<Icon color="#606BEE" as={Search2Icon} />
+			</Box>
+			<Input ml="0" focusBorderColor="#606BEE" placeholder="Enter a City to start searching e.g Lagos...." />
+		</Stack>
+	);
+};
+
+export default Searchbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,24 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Container, Grid } from '@chakra-ui/layout';
+import { useState } from 'react';
+import Sidebar from '../containers/Sidebar';
+import Searchbar from '../components/Searchbar';
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-      </Text>
+const Index = () => {
+	const [activeTempType, setActiveTempType] = useState({
+		celsius: true,
+		fahrenheit: false,
+	});
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+	return (
+		<Container maxWidth="100%" margin="0" padding="0">
+			<Grid templateColumns="1fr 5fr">
+				<Sidebar activeTempType={activeTempType} />
+				<Container maxWidth="100ch">
+          <Searchbar />
+        </Container>
+			</Grid>
+		</Container>
+	);
+};
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+export default Index;
