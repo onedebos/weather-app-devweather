4f7d06c18006cafa47303454b9c441d28acd4254
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2021-06-17 17:16:55 +0100

    chore: give celsius and fahrenheit buttons a role

--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,14 +1,14 @@
-import { Icon, Text, Container, Stack } from '@chakra-ui/react';
+import { Icon, Text, Container, Box, Stack } from '@chakra-ui/react';
 
 
 const SidebarItem = ({ menuItem, active, setIsCelsius, icon }) => {
 	return (
-		<Stack pl="5" cursor="pointer" backgroundColor="whiteAlpha.200" rounded="md" py="3" onClick={setIsCelsius} mt={{lg:"10", base:"3"}} width="100%" color="white" opacity={active ? '1' : '0.5'}>
+		<Box pl="5" role="button" backgroundColor="whiteAlpha.200" rounded="md" py="3" onClick={setIsCelsius} mt={{lg:"10", base:"3"}} width="100%" color="white" opacity={active ? '1' : '0.5'}>
 			<Text fontWeight="semibold">
 				<Icon mr="1" as={icon} /> 
                 {menuItem}
 			</Text>
-		</Stack>
+		</Box>
 	);
 };
 
